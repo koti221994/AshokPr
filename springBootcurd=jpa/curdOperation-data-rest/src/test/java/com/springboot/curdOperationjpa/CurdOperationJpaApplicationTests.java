5a7e8221931805e9ed900a4fcafeffc0d7794835
koti221994 <71209076+koti221994@users.noreply.github.com>
2021-03-20 12:33:45 +0530

    new project

---
+++ springBootcurd=jpa/curdOperation-data-rest/src/test/java/com/springboot/curdOperationjpa/CurdOperationJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.curdOperationjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdOperationJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
